26e6b710b7411417cb4b84c3c6888c3a4752dbca
Taha <taha.alami@um5s.net.ma>
2020-04-24 23:36:15 +0200

    created a register page

---
+++ client/src/views/Register.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="register">
+    <v-container>
+      <v-row class="text-center">
+        <v-col cols="12">
+          <h3>Register</h3>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style  scoped>
+
+</style>
